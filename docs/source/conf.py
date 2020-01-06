997b2f376cd3c98b5754556ba431e85ab820beb9
alexOarga <alex718123@gmail.com>
2020-01-06 23:40:59 +0100

    trying docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,75 +1,57 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+import sphinx_rtd_theme
 # Use sphinx-quickstart to create your own conf.py file!
 # After that, you have to edit a few things.  See below.
 
-# Select nbsphinx and, if needed, other Sphinx extensions:
+# Select nbsphinx and, if needed, add a math extension (mathjax or pngmath):
 extensions = [
     'nbsphinx',
-    'sphinx.ext.mathjax',  # for math equations
-    'sphinxcontrib.bibtex',  # for bibliographic references
-    'sphinxcontrib.rsvgconverter',  # for SVG->PDF conversion in LaTeX output
+    'sphinx.ext.mathjax',
 ]
 
 # Exclude build directory and Jupyter backup files:
 exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
-# Default language for syntax highlighting in reST and Markdown cells:
+# Default language for syntax highlighting in reST and Markdown cells
 highlight_language = 'none'
 
-# Don't add .txt suffix to source files:
+# Don't add .txt suffix to source files (available for Sphinx >= 1.5):
 html_sourcelink_suffix = ''
 
-# Work-around until https://github.com/sphinx-doc/sphinx/issues/4229 is solved:
-html_scaled_image_link = False
+# Execute notebooks before conversion: 'always', 'never', 'auto' (default)
+#nbsphinx_execute = 'never'
+
+# Use this kernel instead of the one stored in the notebook metadata:
+#nbsphinx_kernel_name = 'python3'
 
 # List of arguments to be passed to the kernel that executes the notebooks:
-nbsphinx_execute_arguments = [
-    "--InlineBackend.figure_formats={'svg', 'pdf'}",
-    "--InlineBackend.rc={'figure.dpi': 96}",
-]
+#nbsphinx_execute_arguments = ['--InlineBackend.figure_formats={"png", "pdf"}']
 
-# This is processed by Jinja2 and inserted before each notebook
-nbsphinx_prolog = r"""
-{% set docname = 'docs/' + env.doc2path(env.docname, base=None) %}
+# If True, the build process is continued even if an exception occurs:
+#nbsphinx_allow_errors = True
 
-.. only:: html
+# Controls when a cell will time out (defaults to 30; use -1 for no timeout):
+#nbsphinx_timeout = 60
 
-    .. role:: raw-html(raw)
-        :format: html
+# Default Pygments lexer for syntax highlighting in code cells:
+#nbsphinx_codecell_lexer = 'ipython3'
 
-    .. nbinfo::
+# Width of input/output prompts used in CSS:
+#nbsphinx_prompt_width = '8ex'
 
-        This page was generated from `{{ docname }}`__.
-        Interactive online version:
-        :raw-html:`<a href="https://mybinder.org/v2/gh/spatialaudio/nbsphinx/{{ env.config.release }}?filepath={{ docname }}"><img alt="Binder badge" src="https://mybinder.org/badge_logo.svg" style="vertical-align:text-bottom"></a>`
-
-    __ https://github.com/spatialaudio/nbsphinx/blob/
-        {{ env.config.release }}/{{ docname }}
-
-.. raw:: latex
-
-    \nbsphinxstartnotebook{\scriptsize\noindent\strut
-    \textcolor{gray}{The following section was generated from
-    \sphinxcode{\sphinxupquote{\strut {{ docname | escape_latex }}}} \dotfill}}
-"""
-
-# This is processed by Jinja2 and inserted after each notebook
-nbsphinx_epilog = r"""
-{% set docname = 'docs/' + env.doc2path(env.docname, base=None) %}
-.. raw:: latex
-
-    \nbsphinxstopnotebook{\scriptsize\noindent\strut
-    \textcolor{gray}{\dotfill\ \sphinxcode{\sphinxupquote{\strut
-    {{ docname | escape_latex }}}} ends here.}}
-"""
-
-mathjax_config = {
-    'TeX': {'equationNumbers': {'autoNumber': 'AMS', 'useLabelIds': True}},
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_logo = 'img/logo.svg'
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
 }
 
-# Additional files needed for generating LaTeX/PDF output:
-latex_additional_files = ['references.bib']
+# If window is narrower than this, input/output prompts are on separate lines:
+#nbsphinx_responsive_width = '700px'
 
 # -- The settings below this line are not specific to nbsphinx ------------
 
@@ -77,21 +59,18 @@ master_doc = 'index'
 
 project = 'findCritical'
 author = 'Alex Oarga'
-copyright = '2019, ' + author
+copyright = '2020, ' + author
 
 linkcheck_ignore = [r'http://localhost:\d+/']
 
-# -- Get version information and date from Git ----------------------------
+# -- Get version information from Git -------------------------------------
 
 try:
     from subprocess import check_output
     release = check_output(['git', 'describe', '--tags', '--always'])
     release = release.decode().strip()
-    today = check_output(['git', 'show', '-s', '--format=%ad', '--date=short'])
-    today = today.decode().strip()
 except Exception:
     release = '<unknown>'
-    today = '<unknown date>'
 
 # -- Options for HTML output ----------------------------------------------
 
@@ -99,22 +78,8 @@ html_title = project + ' version ' + release
 
 # -- Options for LaTeX output ---------------------------------------------
 
-# See https://www.sphinx-doc.org/en/master/latex.html
 latex_elements = {
     'papersize': 'a4paper',
-    'printindex': '',
-    'sphinxsetup': r"""
-        %verbatimwithframe=false,
-        %verbatimwrapslines=false,
-        %verbatimhintsturnover=false,
-        VerbatimColor={HTML}{F5F5F5},
-        VerbatimBorderColor={HTML}{E0E0E0},
-        noteBorderColor={HTML}{E0E0E0},
-        noteborder=1.5pt,
-        warningBorderColor={HTML}{E0E0E0},
-        warningborder=1.5pt,
-        warningBgColor={HTML}{FBFBFB},
-    """,
     'preamble': r"""
 \usepackage[sc,osf]{mathpazo}
 \linespread{1.05}  % see http://www.tug.dk/FontCatalogue/urwpalladio/
@@ -124,7 +89,6 @@ latex_elements = {
 }{
     \renewcommand{\ttdefault}{lmtt}  % typewriter font from lmodern
 }
-\usepackage{booktabs}  % for Pandas dataframes
 """,
 }
 
@@ -133,10 +97,3 @@ latex_documents = [
 ]
 
 latex_show_urls = 'footnote'
-latex_show_pagerefs = True
-
-# -- Options for EPUB output ----------------------------------------------
-
-# These are just defined to avoid Sphinx warnings related to EPUB:
-version = release
-suppress_warnings = ['epub.unknown_project_files']
