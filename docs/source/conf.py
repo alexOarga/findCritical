610d7ba3efd9b1b31a4a2e757d63f15b07374828
alexOarga <alex718123@gmail.com>
2020-01-06 23:05:54 +0100

    change docs sphinx version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,9 +42,15 @@ author = 'Alex Oarga'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-	'recommonmark',
-	'nbsphinx',
-    'sphinx.ext.mathjax',
+    'recommonmark',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosummary",
+    "autoapi.extension",
+    "nbsphinx",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
