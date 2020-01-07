3bade2a75b27a6c94462a1425441fb75ab6a5582
alexOarga <alex718123@gmail.com>
2020-01-08 00:45:55 +0100

    indent docs

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'c18f988',
+    VERSION: '6047f2f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
